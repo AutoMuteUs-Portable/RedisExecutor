f31590916e1b244307743cf3563b4e81f5d5495d
mtaku3 <57391256+mtaku3@users.noreply.github.com>
2023-01-13 17:49:05 +0900

    Initial commit

---
+++ AutoMuteUsPortable.Executor/ExecutorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Reactive.Subjects;
+using AutoMuteUsPortable.Shared.Controller.Executor;
+using AutoMuteUsPortable.Shared.Entity.ProgressInfo;
+
+namespace AutoMuteUsPortable.Executor;
+
+public class ExecutorController : IExecutorController
+{
+    public static Dictionary<string, Parameter> InstallParameters = new();
+    public static Dictionary<string, Parameter> UpdateParameters = new();
+
+    public async Task Run(ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Stop(ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Restart(ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Install(Dictionary<string, string> parameters, ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task Update(Dictionary<string, string> parameters, ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task InstallBySimpleSettings(dynamic simpleSettings, dynamic executorConfigurationBase,
+        ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateBySimpleSettings(dynamic simpleSettings, dynamic executorConfigurationBase,
+        ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# ExecutorController
+This is a template implementation of ExecutorController for [AutoMuteUs Portable](https://github.com/mtaku3/AutoMuteUs-Portable)
+This will be built as .dll and dynamically loaded into AutoMuteUs Portable
+
+# How to use this template
+1. Create new repository using this template
+2. Initialize submodule by hitting `git submodule update --init`
+3. Update submodule by hitting `git submodule update –-remote`
+
+We always try to make the submodule up-to-date in this repository but it may not be. Make sure to update submodule by hitting the above command.
