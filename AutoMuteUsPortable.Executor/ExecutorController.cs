25bf8c81bd7f5dc3d9eb82871c34b3b8e6c9ece0
mtaku3 <m.taku3.1222@gmail.com>
2023-02-26 19:59:14 +0900

    fix: fix stop event invoke

--- AutoMuteUsPortable.Executor/ExecutorController.cs
+++ AutoMuteUsPortable.Executor/ExecutorController.cs
@@ -281,15 +281,11 @@ public class ExecutorController : ExecutorControllerBase
                     e =>
                     {
                         if (e is StartedCommandEvent started) OnStart();
-                    }, ex =>
-                    {
-                        if (ex is TaskCanceledException taskCanceledException) OnStop();
-                        // TODO: log out exception
-                    }, OnStop);
+                    }, _ => OnStop(), OnStop);
         }
         catch (OperationCanceledException ex)
         {
-            // ignored
+            OnStop();
         }
         catch
         {
